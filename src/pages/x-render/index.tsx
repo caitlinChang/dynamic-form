07b44714846368659a61a716bceb3a01f9cc98d2
yingying.chang <yingying.chang@shopee.com>
2021-10-11 11:50:41 +0800

    feat: x-render test

---
+++ src/pages/x-render/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FormRenderComponent = function () {
+  return;
+};
+
+export default FormRenderComponent;
---
+++ src/utils/asyncDataSource.ts
@@ -0,0 +1,6 @@
+/** 异步数据源测试 */
+export const getDataSource = function () {
+  return new Promise((resolve, reject) => {
+    return;
+  });
+};
