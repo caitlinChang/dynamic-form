b5fa11c4489b8dcdcc5e89e52cd43e9ccfc9b80f
yingying.chang <yingying.chang@shopee.com>
2021-10-14 14:09:15 +0800

    feat: antd form compare with formily form

--- src/pages/performance/components/CustomFormilyInput/index.tsx
+++ src/pages/performance/components/CustomFormilyInput/index.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { Input } from "antd";
 
-import { observer, useForm } from "@formily/react";
+import { observer, useFormEffects, useForm } from "@formily/react";
+import { onFieldReact, onFieldValueChange } from "@formily/core";
 
-function CustomInput(props: any) {
+type PropsType = {
+  value: string;
+  onChange: (e: any) => void;
+};
+
+function CustomFormilyInput(props: PropsType) {
   console.log("child re-render");
+  const form = useForm();
   // 关联逻辑
-
-  return <Input />;
+  useFormEffects(() => {
+    onFieldReact("case1", (field: any) => {
+      field.value = form.getValuesIn(["case2"]) + "22222";
+    });
+  });
+  return <Input value={props.value} onChange={props.onChange} />;
 }
 
-export default observer(CustomInput);
+export default observer(CustomFormilyInput);
--- src/pages/performance/index.tsx
+++ src/pages/performance/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import CustomInput from "./components/CustomInput";
-import { Form } from "antd";
-import { useForm } from "@formily/react";
-import { Form as FormilyForm } from "@formily/antd";
+import CustomFormilyInput from "./components/CustomFormilyInput";
+import { createForm, onFieldValueChange } from "@formily/core";
+import { Button, Form } from "antd";
+import { useForm, Field } from "@formily/react";
+import { Form as FormilyForm, FormItem } from "@formily/antd";
 
 export default function PerformanceComponent() {
   console.log("parent re-render");
   const [form] = Form.useForm();
-  const formilyForm = useForm();
+  const formilyForm = createForm();
   const handleValueChange = function (changedValues: any) {
     console.log("trigger");
 
@@ -16,6 +18,11 @@ export default function PerformanceComponent() {
         name2: 2,
       });
     }
+  };
+
+  const handleSave = function () {
+    const values = formilyForm.getValuesIn([]);
+    console.log("values = ", values);
   };
   return (
     <div>
@@ -30,7 +37,29 @@ export default function PerformanceComponent() {
       </Form>
 
       <h3>Mobx分布式数据</h3>
-      <FormilyForm form={formilyForm}></FormilyForm>
+      <FormilyForm form={formilyForm}>
+        <Field
+          name="case1"
+          decorator={[
+            FormItem,
+            {
+              label: "Case1",
+            },
+          ]}
+          component={[CustomFormilyInput]}
+        ></Field>
+        <Field
+          name="case2"
+          decorator={[
+            FormItem,
+            {
+              label: "Case2",
+            },
+          ]}
+          component={[CustomFormilyInput]}
+        ></Field>
+        <Button onClick={handleSave}>Save</Button>
+      </FormilyForm>
     </div>
   );
 }
